0b9040b763ed29b8c089950d15a287daeada4a60
Kazuho Shibutani <3i1111416@gmail.com>
2021-03-23 09:48:53 +0900

    Add delete cfn-stack function in scripts.

---
+++ Networking/011-public-subnet-cfn.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+CFN_TEMPLATE=public-subnet-cfn.yaml
+CFN_STACK_NAME=sample-011-public-subnet
+
+CHANGESET_OPTION="--no-execute-changeset"
+EXEC_OPTION="deploy"
+
+if [ $# = 1 ] && [ $1 = "deploy" ]; then
+  echo "deploy mode"
+  CHANGESET_OPTION=""
+elif [ $# = 1 ] && [ $1 = "delete" ]; then
+  echo "delete mode"
+  CHANGESET_OPTION=""
+  EXEC_OPTION="delete-stack"
+  aws cloudformation ${EXEC_OPTION} --stack-name ${CFN_STACK_NAME}
+  exit
+else
+  echo "check change set mode"
+fi
+
+#  テンプレートの実行
+aws cloudformation ${EXEC_OPTION} --stack-name ${CFN_STACK_NAME} --template-file ${CFN_TEMPLATE} ${CHANGESET_OPTION} \
+#  --parameter-overrides \
+#  NameTagPrefix=prd \
+#  VPCCIDR=10.70
---
+++ Networking/bootstrap.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+EXEC_OPTION=""
+
+if [ $# = 1 ] && [ $1 = "deploy" ]; then
+  echo "deploy mode"
+  EXEC_OPTION="deploy"
+#elif [ $# = 1 ] && [ $1 = "delete" ]; then
+#  echo "delete mode"
+#  EXEC_OPTION="delete"
+fi
+
+#  テンプレートの実行
+./010-vpc-cfn.sh ${EXEC_OPTION}
+./011-public-subnet-cfn.sh ${EXEC_OPTION}
+./012-nat-instance-cfn.sh ${EXEC_OPTION}
+./013-private-subnet-cfn.sh ${EXEC_OPTION}
